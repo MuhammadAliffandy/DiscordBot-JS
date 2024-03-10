c2adab62dc9d4829113766ef8279d7f22deea72c
MuhammadAliffandy <aliffandyrealgo@gmail.com>
2024-03-10 20:25:17 +0700

    feat: created logical condition for badword operation

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const Discord = require('discord.js');
 require('dotenv').config();
+const Discord = require('discord.js');
+const badWord = require('./badword.json')
 
 const client = new Discord.Client({ intents: [
     Discord.GatewayIntentBits.Guilds,
@@ -15,16 +16,27 @@ client.on('messageCreate', message => {
     if (message.content === 'oi') {
         message.reply("oi ~~");
     }
-    if (message.content === 'san join') {
-        message.reply('Iya san, sini dong !!');
-    } 
     if (message.content === 'ping') {
         message.reply('Pong~!');
-    } 
+    }
+    
+    const sentence =  message.content.split(' ');
+    
+    if (sentence.length > 1 ) {
+        const isMatch = badWord.some(word => sentence.includes(word));
+        if(isMatch){
+            message.reply('Uhh, Jangan berkata kasar ya ~!'); 
+        }
+    }  
+
+
 })
 
 client.once('ready', () => {
     console.log('The Discord Bot is Ready!');
 })
 
+
 client.login(process.env.BOT_TOKEN)
+
+console.log(badWord[1])
